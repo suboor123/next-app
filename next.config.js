0cf5f06a4db93a6a5d0f639252791fea270b60b9
Suboor Khan <suboork100@gmail.com>
2023-12-22 19:37:10 +0530

    preact mig

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     domains: ['firebasestorage.googleapis.com', 'suboorkhan.com', 'res.cloudinary.com'],
   },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
