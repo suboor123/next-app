ce112b4e783dec058e55b313873f552388115c8d
Suboor Khan <suboorkhan@krapton.com>
2024-09-03 01:39:33 +0530

    changes

--- src/components/LiveSessionPage/LiveSessionCard.jsx
+++ src/components/LiveSessionPage/LiveSessionCard.jsx
@@ -8,7 +8,7 @@ const LiveSessionCard = ({ blog = {} }) => {
 
     return (
         <a className="flex my-5 gap-5 border-dashed items-center border-2 p-3 scale2">
-            <LazyImg src={imageUrl} className="w-[100px] h-[100px] min-w-[100px] min-h-[100px] md:w-[150px] md:h-[150px] md:min-w-[150px] md:min-h-[150px] bg-gray-400 rounded-md object-cover" />
+            <LazyImg src={imageUrl} className="w-[100px] h-[100px] min-w-[100px] min-h-[100px] md:w-[150px] md:h-[150px] md:min-w-[150px] md:min-h-[150px] bg-gray-400 rounded-md object-cover " />
             <div>
                 <h2 className="text-lg md:text-xl font-bold mb-2">{name}</h2>
                 <p className="text-sm font-light multiline-ellipsis-4">{description}</p>
--- src/components/LiveSessionPage/LiveSessionContent.jsx
+++ src/components/LiveSessionPage/LiveSessionContent.jsx
@@ -10,7 +10,7 @@ const LiveSessionContent = ({ sessions = [] }) => {
                 title="Live Sessions & Webinars: Engage with Expert Talks and Tutorials"
                 description="Join me for live sessions and webinars covering the latest trends and techniques in web and mobile development. Participate in interactive discussions, coding tutorials, and Q&A sessions to enhance your skills and stay updated with industry advancements."
             />
-            <div className="bg-slate-100 p-3 my-5 rounded-md">
+            <div className="bg-slate-100 p-3 my-10 rounded-md">
                 <iframe id="session-iframe" className="h-[250px] md:h-[400px] rounded-md" src="https://www.youtube.com/embed/lXFgS8vbej4?enablejsapi=1" width="100%" />
             </div>
             <div className="mt-5 px-5 md:px-0">
