02843213a572d5d0f3cecfa7d21832e49eedd429
Suboor Khan <suboorkhan@krapton.com>
2024-09-14 17:39:48 +0530

    dv

--- src/components/UmamaSuboor/UmamaName.jsx
+++ src/components/UmamaSuboor/UmamaName.jsx
@@ -18,13 +18,15 @@ const UmamaWords = () => {
                         key={index}
                         className='flex gap-4 items-center my-5'
                         initial={{ opacity: 0, x: -50 }} // Initial state: letter starts from the left
-                        animate={{ opacity: 1, x: 0 }} // Animated state: letter moves to its final position
+                        whileInView={{ opacity: 1, x: 0 }} 
+                        viewport={{ once: true }} 
                         transition={{ duration: 0.8, delay: index * 0.3 }} // Staggered animation for each item
                     >
                         <motion.h3
                             className='text-8xl font-extrabold text-pink-500'
                             initial={{ opacity: 0, x: -50 }} // Letter animation: slides in from left
-                            animate={{ opacity: 1, x: 0 }}
+                            whileInView={{ opacity: 1, x: 0 }}
+                            viewport={{ once: true }} 
                             transition={{ duration: 1, delay: index * 0.3 }} // Delayed animation for smooth effect
                         >
                             {w.letter}
@@ -32,8 +34,9 @@ const UmamaWords = () => {
                         <motion.p
                             className='text-2xl text-gray-700'
                             initial={{ opacity: 0, x: 50 }} // Word animation: slides in from right
-                            animate={{ opacity: 1, x: 0 }}
-                            transition={{ duration: 1, delay: index * 0.3 }} // Delayed animation for smooth effect
+                            whileInView={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 1, delay: index * 0.3 }} 
+                            viewport={{ once: true }} 
                         >
                             {w.word}
                         </motion.p>
