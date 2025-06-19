d4d4cb7eb205527afee92608004b339ffb73f659
Suboor Khan <39632489+suboor123@users.noreply.github.com>
2025-06-19 23:04:41 +0530

    changes

--- src/app/umama/suboor/page.js
+++ src/app/umama/suboor/page.js
@@ -4,10 +4,10 @@ import { createMetaData } from '@/seo-utils/CommonMeta';
 import Head from 'next/head';
 
 const url = `${HOST}`;
-const title = 'Umama';
-const description = 'Umama';
-const keywords = 'Umama';
-const image = `${HOST}/assets/umama.jpeg`;
+const title = 'To My Love, Umama ❤️ – A Page from My Heart';
+const description = 'A deeply romantic page dedicated to Umama – filled with love, memories, music, and a promise of forever. Every word here is written just for you.';
+const keywords = 'Umama, love letter to Umama, romantic page, proposal for Umama, Umama and me, love story, heartfelt message, songs for Umama, forever love';
+const image = `${HOST}/assets/su.jpeg`;
 
 // Create metadata object for SEO
 export const metadata = {
