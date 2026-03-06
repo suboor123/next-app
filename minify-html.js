35c85677574067a63c6f50e58e57b3761a04e3de
Suboor Khan <39632489+suboor123@users.noreply.github.com>
2026-03-06 20:23:47 +0530

    changes

--- minify-html.js
+++ minify-html.js
@@ -4,29 +4,42 @@ const { minify } = require("html-minifier-terser");
 
 const directory = "./"; // current directory
 
+const allowMinification = {
+    "index.html": false,
+    "me.html": true,
+    "react-nextjs-developer.html": true,
+    "performance.html": false,
+    "react-nextjs-developer": true
+}
+
 async function minifyHtmlFiles() {
   const files = fs.readdirSync(directory);
 
   for (const file of files) {
-    if (path.extname(file) === ".html") {
+    if(!allowMinification[file]) {}
+    else {
+        if (path.extname(file) === ".html") {
 
-      const filePath = path.join(directory, file);
-
-      const html = fs.readFileSync(filePath, "utf8");
-
-      const minified = await minify(html, {
-        collapseWhitespace: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        removeEmptyAttributes: true,
-        minifyCSS: true,
-        minifyJS: true
-      });
-
-      fs.writeFileSync(filePath, minified);
-
-      console.log(`Minified: ${file}`);
+            const filePath = path.join(directory, file);
+      
+            const html = fs.readFileSync(filePath, "utf8");
+      
+            const minified = await minify(html, {
+              collapseWhitespace: true,
+              removeComments: true,
+              removeRedundantAttributes: true,
+              removeEmptyAttributes: true,
+              minifyCSS: true,
+              minifyJS: true
+            });
+      
+            fs.writeFileSync(filePath, minified);
+      
+            console.log(`Minified: ${file}`);
+          }
     }
+  
+
   }
 
   console.log("All HTML files minified.");
